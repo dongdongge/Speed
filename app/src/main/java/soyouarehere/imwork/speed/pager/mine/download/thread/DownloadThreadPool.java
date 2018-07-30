b3a46d2a3dcd6d962a333024a868efe44d28e9fe
lxd <625324231@qq.com>
2018-07-30 22:43:15 +0800

    创建下载管理

--- app/src/main/java/soyouarehere/imwork/speed/pager/mine/download/thread/DownloadThreadPool.java
+++ app/src/main/java/soyouarehere/imwork/speed/pager/mine/download/thread/DownloadThreadPool.java
@@ -1,5 +1,7 @@
 package soyouarehere.imwork.speed.pager.mine.download.thread;
 
+import android.app.DownloadManager;
+
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadFactory;
@@ -7,7 +9,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class DownloadThreadPool extends ThreadPoolExecutor {
-
 
     public DownloadThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
