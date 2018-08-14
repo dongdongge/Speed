a072e1c7210f6e63709b2ec032f17bf7433c62a7
dongdongge <625324231@qq.com>
2018-08-14 18:01:48 +0800

    完成服务器资源列表

--- app/src/main/java/soyouarehere/imwork/speed/pager/mine/download/resouce/ServiceResourceActivity.java
+++ app/src/main/java/soyouarehere/imwork/speed/pager/mine/download/resouce/ServiceResourceActivity.java
@@ -1,8 +1,10 @@
 package soyouarehere.imwork.speed.pager.mine.download.resouce;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -10,10 +12,16 @@ import soyouarehere.imwork.speed.R;
 import soyouarehere.imwork.speed.app.base.mvp.BaseActivity;
 import soyouarehere.imwork.speed.util.log.LogUtil;
 
-public class ServiceResourceActivity extends BaseActivity<ServiceResourcePresenter,ServiceResourceModel> implements ServiceResourceContract.View{
+/**
+ * @author li.xiaodong
+ * @desc
+ * @time 2018/8/14 16:30
+ */
+public class ServiceResourceActivity extends BaseActivity<ServiceResourcePresenter, ServiceResourceModel> implements ServiceResourceContract.View {
 
     @BindView(R.id.service_rcv_resource_list)
     RecyclerView recyclerView;
+
     @Override
     public void DataLoadError() {
 
@@ -24,19 +32,35 @@ public class ServiceResourceActivity extends BaseActivity<ServiceResourcePresent
         return R.layout.activity_service_resource;
     }
 
+    List<ResourceFile> resourceFiles;
+    ServiceResourceAdapter serviceResourceAdapter;
+
     @Override
     public void create(Bundle savedInstanceState) {
+        showLoading();
         mPresenter.getServiceResourceList();
+        resourceFiles = new ArrayList<>();
+        serviceResourceAdapter = new ServiceResourceAdapter(this, resourceFiles, new ServiceResourceAdapter.ServiceResourceListener() {
+            @Override
+            public void callBack(ResourceFile file) {
+                LogUtil.e("你点击了", file.toString());
+            }
+        });
+        recyclerView.setAdapter(serviceResourceAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     public void success(List list) {
-        List<ResourceFile> list1  = list;
-        LogUtil.e("接受到数据",list.toString(),"大小",list.size());
+        dissLoading();
+        resourceFiles.clear();
+        resourceFiles.addAll(list);
+        serviceResourceAdapter.updateAll();
+        LogUtil.e("接受到数据", list.toString(), "大小", list.size());
     }
 
     @Override
     public void fail(String errMsg) {
-
+        dissLoading();
     }
 }
--- app/src/main/java/soyouarehere/imwork/speed/pager/mine/download/resouce/ServiceResourceAdapter.java
+++ app/src/main/java/soyouarehere/imwork/speed/pager/mine/download/resouce/ServiceResourceAdapter.java
@@ -2,10 +2,15 @@ package soyouarehere.imwork.speed.pager.mine.download.resouce;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.List;
 
+import soyouarehere.imwork.speed.R;
 import soyouarehere.imwork.speed.app.adapter.recycle_view.RecyclerBaseAdapter;
 import soyouarehere.imwork.speed.app.adapter.recycle_view.ViewHolder;
 
@@ -14,18 +19,36 @@ import soyouarehere.imwork.speed.app.adapter.recycle_view.ViewHolder;
  */
 
 public class ServiceResourceAdapter extends RecyclerBaseAdapter<ResourceFile> {
-
-    protected ServiceResourceAdapter(@NonNull Context context, @NonNull List<ResourceFile> mDataList) {
+    ServiceResourceListener serviceResourceListener;
+    protected ServiceResourceAdapter(@NonNull Context context, @NonNull List<ResourceFile> mDataList,ServiceResourceListener serviceResourceListener) {
         super(context, mDataList);
+        this.serviceResourceListener = serviceResourceListener;
     }
 
     @Override
     protected void bindDataForView(ViewHolder holder, ResourceFile resourceRe, int position) {
-
+        TextView fileName = holder.getView(R.id.service_resource_file_name);
+        TextView fileSize = holder.getView(R.id.service_resource_file_size);
+        TextView fileUrl = holder.getView(R.id.service_resource_file_url);
+        fileName.setText(resourceRe.getName());
+        fileSize.setText(resourceRe.getFileLength()+"B");
+        fileUrl.setText(resourceRe.getUrl());
+        ImageButton imageButton = holder.getView(R.id.service_resource_download);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceResourceListener.callBack(resourceRe);
+            }
+        });
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.service_resource_activity_adapter_list_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    public interface ServiceResourceListener{
+        void callBack(ResourceFile file);
     }
 }
